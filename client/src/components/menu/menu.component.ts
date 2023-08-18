75ae79c2755c7eb8109dd00b8504a274e9bc9e86
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-08-18 01:33:45 -0300

    feat: "Fixed responsiveness for menu component and users page"

--- client/src/components/menu/menu.component.ts
+++ client/src/components/menu/menu.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
 export class MenuComponent {
+  isMenuVisible = false;
 
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  faBars = faBars;
 }
